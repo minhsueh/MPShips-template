fc4aab8eb0bde847cfef02516bb0513dee783a16
Chiu Peter <chiupeter@Chius-MBP-2.dhcp.lbl.gov>
2025-10-08 09:30:42 -0700

    add basic stuff

---
+++ mpships_app/__init__.py
@@ -0,0 +1,2 @@
+__author__ = """cool_app_author"""
+__email__ = "cool_app_author@gmail.com"
--- mpships_app/app.py
+++ mpships_app/app.py
@@ -2,12 +2,13 @@ __description__ = "This app show some cool stuff."
 __author__ = "cool_app_author"
 
 
-from dash import Dash, html, dcc, callback, Output, Input, MATCH, State, ctx
+from dash import Dash, html
+
 
 class CoolAPP(html.Div):
-    
+    pass
 
 
 if __name__ == "__main__":
     app = Dash(__name__, suppress_callback_exceptions=True, use_pages=False)
-    app.run_server(host='0.0.0.0', port=8051, debug=True)
+    app.run_server(host="0.0.0.0", port=8051, debug=True)
